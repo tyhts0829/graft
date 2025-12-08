80abff7e365913eb7d19b8fd05d68e02b0cb15fb
tyhts0829 <tyhts0829@gmail.com>
2025-12-08 18:38:25 +0900

    📝

--- main.py
+++ main.py
@@ -2,14 +2,23 @@ import sys
 
 sys.path.append("src")
 
-from api import E, G
+from api import E, G, run
 from core.realize import realize
 
-c = G.circle(r=2.0, segments=8)
-print(c)
-s = E.scale(s=0.5)
-scaled_c = s(c)
-print(scaled_c)
 
-realized_c = realize(scaled_c)
-print(realized_c)
+def draw(t: float) -> None:
+    c = G.circle(r=1.0, segments=16)
+    s = E.scale(s=1.0 + 0.5 * t)
+    scaled_c = s(c)
+    return scaled_c
+
+
+if __name__ == "__main__":
+    run(
+        draw,
+        background_color=(1.0, 1.0, 1.0, 1.0),
+        line_thickness=0.005,
+        line_color=(0.0, 0.0, 0.0, 1.0),
+        render_scale=6.0,
+        canvas_size=(800, 800),
+    )
